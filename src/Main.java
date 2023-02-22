2f00682c609f718a4e3d87db9ad33b939bea2422
dietrich1208 <62686652+dietrich1208@users.noreply.github.com>
2023-02-22 10:44:08 +0200

    Realized Task3 with Car

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import task3.Car;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car();
+        car.start();
+
+
     }
 }
